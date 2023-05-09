d22aaa8512d8754f7e28b0a7b56a8abe2e78924f
ErgulErtugrul <ertugrulergul.7@gmail.com>
2023-05-09 22:01:07 +0300

    v2

---
+++ src/main/java/day05concatinationoperatorstypecasting/Concatination.java
@@ -0,0 +1,26 @@
+package day05concatinationoperatorstypecasting;
+
+import javax.lang.model.SourceVersion;
+
+public class Concatination {
+    public static void main(String[] args) {
+        //Ornek 1: Bir string ve iki integer variable olusturun. String degeri ile Integer larin toplamini console a yazdiirin
+        String  s = "elma";
+        int a = 10;
+        int b = 11;
+        System.out.println(s+a+b);//1011
+        System.out.println(s+(a+b));//elma21
+        System.out.println(s+a*b);//elma110
+        System.out.println("a+b+s");//21elma
+        System.out.println(a+s+b);//10elma11
+        //Java da + sembolü iki sayı arasında kullanılırsa toplama işlemi olur
+        //Java da
+
+
+
+
+
+
+
+    }
+}
