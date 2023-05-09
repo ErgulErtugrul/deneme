7c66328b10a366a3a84bbaa3130dd19971e7f887
ErgulErtugrul <ertugrulergul.7@gmail.com>
2023-05-09 20:57:48 +0300

    first

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -3,6 +3,8 @@ public class Class1 {
         //Version 1 çalışmaları...
         //Version 2 kodları
         //version 4
+        //git hub değişiklikleri
+
 
 
 
---
+++ src/main/java/day04scannerwrapper/Scanner02.java
@@ -0,0 +1,34 @@
+package day04scannerwrapper;
+
+import java.util.Scanner;
+
+public class Scanner02 {
+    public static void main(String[] args) {
+        //Örnek; kullanıcıdan adresini alınız ve ekrana yazdırınız
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen adresinizi giriniz..");
+
+        String adress =  input.nextLine();
+        System.out.println(adress);
+        //nextline() methodu kullanıcıdan çok kelimeli string için kullanılır
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
